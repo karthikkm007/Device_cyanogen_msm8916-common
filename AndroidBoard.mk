c251f688957141fbde5b61361ce2576d3be35a72
karthikkm007 <jacksparrow941994@gmail.com>
2018-03-27 17:29:51 +0530

    wt88047:somthing is changes to get camera and volte

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,3 +4,6 @@ LOCAL_PATH := $(call my-dir)
 # extra images
 #----------------------------------------------------------------------
 include build/core/generate_extra_images.mk
+
+# Copy firmware
+include device/wingtech/wt88047/radio.mk
